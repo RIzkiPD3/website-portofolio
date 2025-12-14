1f89c9ee6288ba4520d9f018f106b7684e1528e6
RizkiPD3 <riskiputradandi@gmail.com>
2025-12-14 22:06:29 +0700

    about: add about section with profile and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 // app/page.tsx
+import AboutSection from "@/components/about-section";
 import Hero from "@/components/hero";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <AboutSection />
     </>
   );
 }
